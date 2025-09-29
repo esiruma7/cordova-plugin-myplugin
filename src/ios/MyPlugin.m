08ccbe76edd05c1b6a1728d3df87ed0072d91e1f
Edwin Resuello Siruma <edwinsiruma@fareast.com>
2025-09-29 11:40:17 +0800

    Updated 29 Sep 11:30am

--- src/ios/MyPlugin.m
+++ src/ios/MyPlugin.m
@@ -5,10 +5,12 @@
 @end
 
 @implementation MyPlugin
+
 - (void)sayHello:(CDVInvokedUrlCommand*)command {
     NSString* msg = [command.arguments objectAtIndex:0];
-    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                               messageAsString:[@"Hello " stringByAppendingString:msg]];
+    NSString* response = [NSString stringWithFormat:@"Hello from iOS: %@", msg];
+
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:response];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 @end
