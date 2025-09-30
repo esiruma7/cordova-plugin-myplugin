72c12a33c6c85b252d765dee48f98c805b0abbb3
Edwin Resuello Siruma <edwinsiruma@fareast.com>
2025-09-30 16:52:21 +0800

    Updated MyPlugin.js

--- www/MyPlugin.js
+++ www/MyPlugin.js
@@ -1,13 +1,38 @@
 var exec = require('cordova/exec');
 
-module.exports = {
+var MyPlugin = {
+    /**
+     * Simple Hello World test
+     * Usage:
+     *   window.MyPlugin.sayHello("World",
+     *       function(res) { alert("OK: " + res); },
+     *       function(err) { alert("ERR: " + err); });
+     */
     sayHello: function (name, success, error) {
         exec(success, error, "MyPlugin", "sayHello", [name]);
     },
+
+    /**
+     * Test whether LEGIC SDK classes are accessible
+     * Usage:
+     *   window.MyPlugin.testLegic(
+     *       function(res) { alert("OK: " + res); },
+     *       function(err) { alert("ERR: " + err); });
+     */
     testLegic: function (success, error) {
         exec(success, error, "MyPlugin", "testLegic", []);
     },
+
+    /**
+     * Initialize LEGIC SDK (if available)
+     * Usage:
+     *   window.MyPlugin.initLegic(appId, user, pass, url,
+     *       function(res) { alert("OK: " + res); },
+     *       function(err) { alert("ERR: " + err); });
+     */
     initLegic: function (appId, user, pass, url, success, error) {
         exec(success, error, "MyPlugin", "initLegic", [appId, user, pass, url]);
     }
 };
+
+module.exports = MyPlugin;
